c2bae742f9564ba2aa92e6db80ee547db4faa045
MacneDav <imperiment@gmail.com>
2026-07-26 10:46:36 +0200

    Implementation of basic REPL in C++

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,42 +1,30 @@
-#include "String.h"
-#include "ReadLine.h"
-
 #include <iostream>
-#include <memory>
+#include <string>
 
-String READ(const String& input);
-String EVAL(const String& ast);
-String PRINT(const String& ast);
-String rep(const String& input);
-
-static ReadLine s_readLine("~/.mal-history");
-
-int main(int argc, char* argv[])
-{
-    String prompt = "user> ";
-    String input;
-    while (s_readLine.get(prompt, input)) {
-        std::cout << rep(input) << "\n";
-    }
-    return 0;
-}
-
-String rep(const String& input)
-{
-    return PRINT(EVAL(READ(input)));
-}
-
-String READ(const String& input)
-{
+std::string READ(const std::string &input) {
     return input;
 }
-
-String EVAL(const String& ast)
-{
-    return ast;
+std::string EVAL(const std::string &input) {
+    return input;
+}
+std::string PRINT(const std::string &input) {
+    return input;
+}
+std::string rep(std::string &input) {
+    auto ast = READ(input);
+    auto res = EVAL(ast);
+    return PRINT(res);
 }
 
-String PRINT(const String& ast)
-{
-    return ast;
+int main() {
+    std::string line;
+    for (;;) {
+        std::cout << "user> ";
+        if (!std::getline(std::cin, line)) {
+            break;
+        }
+        std::cout << rep(line) << '\n';
+    }
+
+    return 0;
 }
